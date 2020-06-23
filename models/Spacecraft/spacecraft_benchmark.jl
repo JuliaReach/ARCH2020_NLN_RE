f6dade433d9b978bfda430460edd241167308adf
mforets <mforets@gmail.com>
2020-06-23 13:55:45 -0300

    update model

--- models/Spacecraft/spacecraft_benchmark.jl
+++ models/Spacecraft/spacecraft_benchmark.jl
@@ -1,24 +1,26 @@
+include("spacecraft.jl")
+
 function solve_spacecraft(prob; k=25, s=missing)
 
     # transition from mode 1 to mode 2
     sol12 = solve(prob,
                 tspan=(0.0, 200.0),
-                alg=TMJets(abs_tol=1e-5, max_steps=10_000, orderT=5, orderQ=1, disjointness=BoxEnclosure()),
+                alg=TMJets(abs_tol=1e-5, max_steps=10_000, orderT=5, orderQ=1, disjointness=RA.BoxEnclosure()),
                 max_jumps=1,
                 intersect_source_invariant=false,
-                intersection_method=TemplateHullIntersection(boxdirs),
-                clustering_method=LazyClustering(1),
-                disjointness_method=BoxEnclosure())
+                intersection_method=RA.TemplateHullIntersection(boxdirs),
+                clustering_method=RA.LazyClustering(1),
+                disjointness_method=RA.BoxEnclosure())
 
     sol12jump = overapproximate(sol12[2](120 .. 150), Zonotope)
     t0 = tstart(sol12jump[1])
-    sol12jump_c = cluster(sol12jump, 1:length(sol12jump), BoxClustering(k, s))
+    sol12jump_c = cluster(sol12jump, 1:length(sol12jump), RA.BoxClustering(k, s))
 
     # transition from mode 2 to mode 3
     H = system(prob)
     sol3 = solve(IVP(mode(H, 3), [set(X) for X in sol12jump_c]),
                  tspan=(t0, 200.0),
-                 alg=TMJets(abs_tol=1e-10, orderT=7, orderQ=1, disjointness=BoxEnclosure()))
+                 alg=TMJets(abs_tol=1e-10, orderT=7, orderQ=1, disjointness=RA.BoxEnclosure()))
     d = Dict{Symbol, Any}(:loc_id => 3)
 
     return HybridFlowpipe(vcat([fp for fp in sol12.F],
--- startup.jl
+++ startup.jl
@@ -26,10 +26,14 @@ function main()
     # Quadrotor benchmark
     println("###\nRunning Quadrotor benchmark\n###")
     include("models/Quadrotor/quadrotor_benchmark.jl")
-    
+
     # Lotka-Volterra tangential crossing benchmark
     println("###\nRunning Lotka-Volterra tangential crossing benchmark\n###")
     include("models/LotkaVolterra/lotka_volterra_benchmark.jl")
+
+    # Spacecraft benchmark
+    println("###\nSpacecraft benchmark\n###")
+    include("models/Spacecraft/spacecraft_benchmark.jl")
 
     print(io, "\n")
     println("Finished running benchmarks.")
